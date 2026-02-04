13dd688ce67a71210c48f97d879de793b32cd5aa
Abbas Rafiq <abbasrafiq82@gmail.com>
2026-02-04 12:26:40 +0500

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Card from './components/Card'
+import User from './components/Users'
+
+
+const App = () => {
+
+  const arr = [10, 20, 30, 40, 50]
+
+  return (
+    <div className="parent">
+
+      {arr.map(function(elem){
+        return <h1>{elem}</h1>
+      })}
+
+    </div>
+  ) 
+}
+
+export default App
---
+++ src/components/Users.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Users = (props) => {
+  return (
+    <div style={{color: "white"}}>
+      {props.name}
+    </div>
+  )
+}
+
+export default Users
